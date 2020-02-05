1156ab7c4d37491422e7a7f254d99d01c4396c36
Emmanuel <oluwaseunokeowo2@gmail.com>
2020-02-05 21:14:04 +0100

    added student page

---
+++ student.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Student Page</title>
+    <link rel="stylesheet" href="">
+  </head>
+  <body>
+<h1> Coool </h1>
+  </body>
+</html>
