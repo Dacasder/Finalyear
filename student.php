ca5bb1588b13a10428045143a90d9caa3fc11c54
Emmanuel <oluwaseunokeowo2@gmail.com>
2020-02-11 23:06:28 +0100

    Student page Commit

--- student.php
+++ student.php
@@ -3,9 +3,208 @@
   <head>
     <meta charset="utf-8">
     <title>Student Page</title>
-    <link rel="stylesheet" href="">
+    <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900">
+      <link rel="stylesheet" href="fonts/icomoon/style.css">
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/magnific-popup.css">
+      <link rel="stylesheet" href="css/jquery-ui.css">
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <link rel="stylesheet" href="css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+
+      <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+      <link rel="stylesheet" href="css/aos.css">
+      <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+
+      <link rel="stylesheet" href="css/style.css">
+
+
   </head>
   <body>
-<h1> Coool </h1>
+
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+<div id="overlayer"></div>
+<div class="loader">
+<div class="spinner-border text-primary" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+</div>
+<div class="site-wrap">
+  <div class="site-mobile-menu site-navbar-target">
+  <div class="site-mobile-menu-header">
+    <div class="site-mobile-menu-close mt-3">
+      <span class="icon-close2 js-menu-toggle"></span>
+    </div>
+  </div>
+  <div class="site-mobile-menu-body"></div>
+</div> <!-- .site-mobile-menu -->
+
+<div class="site-navbar-wrap">
+  <div class="site-navbar-top">
+    <div class="container py-3">
+      <div class="row align-items-center">
+      <!--  <div class="col-6">
+       <a href="#" class="p-2 pl-0"><span class="icon-twitter"></span></a>
+          <a href="#" class="p-2 pl-0"><span class="icon-facebook"></span></a>
+          <a href="#" class="p-2 pl-0"><span class="icon-linkedin"></span></a>
+          <a href="#" class="p-2 pl-0"><span class="icon-instagram"></span></a>
+
+        <h4 class="d-none d-md-inline-block" class="d-flex align-items-center">Guardian Angel </h4>
+        <span class="sub-text mb-3 d-block"><em>A Student Academic Monitoring System (S.A.M.S).</em></span>
+      </div>
+    -->
+    <div class="col-6">
+          <div class="d-flex ml-auto">
+          <!--  <a href="#" class="d-flex align-items-center ml-auto mr-4">
+              <span class="icon-envelope mr-2"></span>
+              <span class="d-none d-md-inline-block">no@email.com</span>
+            </a>
+            <a href="#" class="d-flex align-items-center">
+              <span class="icon-phone mr-2"></span>
+              <span class="d-none d-md-inline-block">+234 815 590 1595</span>
+
+
+               class="my-0 site-logo"><a href="index.html"
+
+
+
+            </a> -->
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="site-navbar site-navbar-target js-sticky-header">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-2">
+          <h4 class="d-none d-md-inline-block" class="d-flex align-items-center">Student Page</h4>
+        </div>
+        <div class="col-10">
+          <nav class="site-navigation text-right" role="navigation">
+            <div class="container">
+              <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+
+              <ul class="site-menu main-menu js-clone-nav d-none d-lg-block">
+                <li>
+                  <a href="#home-section" class="nav-link">Home</a>
+                </li>
+              <!--  <li class="has-children">
+                  <a href="#about-section" class="nav-link">About Us</a>
+                  <ul class="dropdown arrow-top">
+                    <li><a href="#our-team-section" class="nav-link">Team</a></li>
+                    <li><a href="#faq-section" class="nav-link">FAQ</a></li>
+                    <li class="has-children">
+                      <a href="#">More Links</a>
+                      <ul class="dropdown">
+                        <li><a href="#">Menu One</a></li>
+                        <li><a href="#">Menu Two</a></li>
+                        <li><a href="#">Menu Three</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>-->
+                <li>
+                  <a href="#projects-section" class="nav-link">Scedual</a>
+                </li>
+                <li><a href="login.html" class="nav-link">Logout</a></li>
+                <li><a href="register.html" class="nav-link">Assinment</a></li>
+                <li class="has-children">
+                  <a href="#contact-section" class="nav-link">Contact</a>
+                  <ul class="dropdown arrow-top">
+                    <li><a href="#our-team-section" class="nav-link">Student Support</a></li>
+                    <li><a href="#faq-section" class="nav-link">School Advisor</a></li>
+                  </ul>
+
+                </li>
+
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="site-blocks-cover overlay" style="background-image: url(img/xxx.jpg);" data-aos="fade" data-stellar-background-ratio="0.5"id="home-section">
+  <div class="container">
+    <div class="row align-items-center text-center justify-content-center">
+      <div class="col-md-8">
+      <!--  <a data-fancybox data-ratio="2" href="" class="play-button d-block">
+          <span class="icon-play"></span>
+        </a>
+        <h1 class="text-uppercase">Guardian Angel </h1>
+        <span class="sub-text mb-3 d-block"><em>A Student Academic Monitoring System (S.A.M.S).</em></span>
+      -->
+      </div>
+    </div>
+  </div>
+</div>
+<div class="site-section  border-bottom">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-4 col-md-6 mb-5 mb-lg-0">
+        <div class="media custom-media">
+          <div class="mr-3 icon"><span class="flaticon-window display-4"></span></div>
+          <div class="media-body">
+            <h5 class="mt-0">XXXXXXX</h5>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin..
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-lg-4 col-md-6 mb-5 mb-lg-0">
+        <div class="media custom-media">
+          <div class="mr-3 icon"><span class="flaticon-sit-down display-4"></span></div>
+          <div class="media-body">
+            <h5 class="mt-0">XXXXXXXX</h5>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin..
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-5 mb-lg-0">
+        <div class="media custom-media">
+          <div class="mr-3 icon"><span class="flaticon-turned-off display-4"></span></div>
+          <div class="media-body">
+            <h5 class="mt-0">XXXXXXXXX</h5>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin..
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+
+</div>
+
+<script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/main.js"></script>
+
   </body>
 </html>
